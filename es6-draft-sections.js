07024c39c4e08ec1c8a0bba926f7ff3c83c6b453
Jason Orendorff <jason.orendorff@gmail.com>
2013-11-12 16:48:24 -0600

    Adjust the script to fire after the page loads. Fixes #41.

--- es6-draft-sections.js
+++ es6-draft-sections.js
@@ -1,8 +1,8 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function (event) {
 var legacy_sections = {
 };
 
     var h = document.location.hash;
     if (h && h.charAt(0) == '#' && legacy_sections.hasOwnProperty(h.slice(1)))
         document.location.hash = legacy_sections[h.slice(1)];
-})();
+});
